0e5fe8c9504a7b8eb4b4fd6ce563a2ac3e60bde6
Polina Petrushina <upcode.121@gmail.com>
2023-05-18 17:07:58 +0300

    Initial commit

---
+++ src/main/java/ru/petrush/CrudSpringBoot/dao/UserDaoImp.java
@@ -0,0 +1,53 @@
+package ru.petrush.CrudSpringBoot.dao;
+
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ru.petrush.CrudSpringBoot.model.User;
+
+import java.util.List;
+
+
+@Component
+public class UserDaoImp implements UserDao {
+
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+    @Autowired
+    public UserDaoImp(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<User> listUsers() {
+        TypedQuery<User> query = entityManager.createQuery("select u from User u", User.class);
+        return query.getResultList();
+    }
+
+
+    public User showUser(long id) {
+        TypedQuery<User> query = entityManager.createQuery("select u from User u where u.id = :id", User.class);
+        query.setParameter("id", id);
+        return query.getResultList().stream().findAny().orElse(null);
+    }
+
+    @Override
+    public void add(User user) {
+        entityManager.persist(user);
+    }
+
+
+    @Override
+    public void update(long id, User user) {
+        entityManager.merge(user);
+    }
+
+    @Override
+    public void delete(long id) {
+        entityManager.remove(entityManager.find(User.class, id));
+    }
+}
